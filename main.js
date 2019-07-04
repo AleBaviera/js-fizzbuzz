fae328ed04ffd84a2de9abb8ea9de5eeeebf50be
alebaviera <baviera.alessandra@gmailcom>
2019-07-04 13:25:54 +0200

    prova struttura js

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('uè');
+
+//
+// Fizz Buzz test: Scrivi un programma che stampi i numeri da 1 a 100 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+// Esattamente quelle devono essere le stringhe di output.
+//
+// *NOTE=* fate output in console -
+
+
+// creo un array con num da 1 a 100
+
+// attraverso l'array:
+// imposto condizione multiplo di 3 e stampo fizz
+
+
+// imposto condizione multiplo di 5 e stampo buzz
+
+
+// se entrambe condizioni : fizz + buzz
