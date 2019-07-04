febbd4125f8a3ceec3010287ea97ab214447d3d1
alebaviera <baviera.alessandra@gmailcom>
2019-07-04 14:36:57 +0200

    FizzBuzz

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ console.log('uè');
 //
 // *NOTE=* fate output in console -
 // dichiaraz var
+var fizz, buzz;
 
 // ciclo da 1 a 100:
 
@@ -14,18 +15,26 @@ for (var i = 1; i < 100; i++){
 
 // console.log(i);
   // imposto condizione multiplo di 3 e stampo fizz
-  if (i % 3 == 0){
-    i = 'Fizz';
-  }
 
+  if (i % 3 == 0 && i % 5 != 0){
+    console.log('Fizz');
+
+
+  }
   // imposto condizione multiplo di 5 e stampo buzz
 
-  else if(i % 5 == 0){
-    i = 'Buzz';
+  else if(i % 5 == 0 && i % 3 != 0){
+    console.log('Buzz') ;
+
+
   }
   // se entrambe condizioni : fizz + buzz
-
+  else if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
+  }
 
 
 console.log(i);
+
+
 }
